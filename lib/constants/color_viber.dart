0d715ad50a21223fef3a0a24a0e10e0962d6e708
Hana <afshari.hln@gmail.com>
2022-10-21 21:08:01 +0330

    update Class Colors

--- lib/constants/color_viber.dart
+++ lib/constants/color_viber.dart
@@ -1,62 +1,50 @@
-
 import 'package:flutter/animation.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/container.dart';
 
 class haSolidColors {
-static Color badg = Color.fromARGB(255, 247, 245, 245); 
+  static Color badg = Color.fromARGB(255, 247, 245, 245);
 
+  static Color unselectedBottomBar = Color.fromARGB(255, 189, 180, 180);
 
+  static Color colorBackgroundAppBarNestedd =
+      Color.fromARGB(255, 252, 253, 253);
 
- static  Color colorIconViber = Color.fromARGB(255, 0, 0, 0);
- static   Color lightScafoldBackgroundColor = Color.fromARGB(255, 255, 255, 255);
- static Color darkScafoldBackgroundColor = Color.fromARGB(255, 41, 40, 40);
+  static Color colorBuyCredit = Color.fromARGB(255, 235, 228, 228);
 
-
-     static Color backgroundColorFloatingAction =Color.fromARGB(255, 105, 4, 238);
-
-    static  Color unselectedBottomBar = Color.fromARGB(255, 189, 180, 180);
-static Color colorBackgroundBottomBar = Color.fromARGB(255, 252, 253, 253);
- static Color colorBackgroundAppBarNestedd = Color.fromARGB(255, 252, 253, 253);
-
-
-static Color colorBuyCredit = Color.fromARGB(255, 235, 228, 228);
-
-
-static Color colorSelectItem =Color.fromARGB(255, 191, 4, 238);
-static Color colorUnSelectItem =Color.fromARGB(255, 104, 35, 194);
-
-
-
+  static Color colorSelectItem = Color.fromARGB(255, 191, 4, 238);
+  static Color colorUnSelectItem = Color.fromARGB(255, 104, 35, 194);
 
 //Allbackground colors in projects
-static Color colorBackgroundAppBarViewAll = Color.fromARGB(255, 235, 228, 228);
-static Color backgroundColorInvite = Color.fromARGB(255, 104, 5, 190);
-static Color backgroundColorLightModeTheme = Color.fromARGB(255, 221, 233, 235);
-static Color backgroundColorDarkModeTheme = Color.fromARGB(255, 55, 57, 57);
-static Color backgroundColorCircularShape = Color.fromARGB(255, 104, 5, 190);
-static Color backgroundColormultiply = Color.fromARGB(255, 156, 154, 158);
-static Color backgroundColorVideoCall = Color.fromARGB(255, 189, 180, 180);
-static Color backgroundColorCall = Color.fromARGB(255, 189, 180, 180);
-static Color backgroundColorInviteContact = Color.fromARGB(255, 189, 180, 180);
-static Color backgroundColorCallsContact = Color.fromARGB(255, 189, 180, 180);
-static Color backgroundColoreInvite = Color.fromARGB(255, 252, 253, 253);
-static Color colorBackgroundAppBarMyNote = Color.fromARGB(255, 252, 253, 253);
-static Color colorBackgroundViberOut = Color.fromARGB(255, 238, 247, 245);
+  static Color colorBackgroundAppBarViewAll =
+      Color.fromARGB(255, 235, 228, 228);
+  static Color backgroundColorInvite = Color.fromARGB(255, 104, 5, 190);
+  static Color backgroundColorLightModeTheme =
+      Color.fromARGB(255, 221, 233, 235);
+  static Color backgroundColorDarkModeTheme = Color.fromARGB(255, 55, 57, 57);
+  static Color backgroundColorCircularShape = Color.fromARGB(255, 104, 5, 190);
+  static Color backgroundColormultiply = Color.fromARGB(255, 156, 154, 158);
+  static Color backgroundColorVideoCall = Color.fromARGB(255, 189, 180, 180);
+  static Color backgroundColorCall = Color.fromARGB(255, 189, 180, 180);
+  static Color backgroundColorInviteContact =
+      Color.fromARGB(255, 189, 180, 180);
+  static Color backgroundColorCallsContact = Color.fromARGB(255, 189, 180, 180);
+  static Color backgroundColoreInvite = Color.fromARGB(255, 252, 253, 253);
+  static Color colorBackgroundAppBarMyNote = Color.fromARGB(255, 252, 253, 253);
+  static Color colorBackgroundViberOut = Color.fromARGB(255, 238, 247, 245);
+  static Color colorBackgroundBottomBar = Color.fromARGB(255, 252, 253, 253);
+  static Color backgroundColorFloatingAction = Color.fromARGB(255, 105, 4, 238);
+  static Color lightScafoldBackgroundColor = Color.fromARGB(255, 255, 255, 255);
+  static Color darkScafoldBackgroundColor = Color.fromARGB(255, 41, 40, 40);
 
+  static Color textColorViewAll = Color.fromARGB(255, 212, 3, 240);
 
-
-static Color textColorViewAll = Color.fromARGB(255, 212, 3, 240);
-
-
-static Color colorIconBack = Color.fromARGB(255, 104, 5, 190);
-static Color colorIconSearch = Color.fromARGB(255, 104, 5, 190);
- static Color colorIconCamera = Color.fromARGB(255, 104, 5, 190);
-static Color colorIconcontact = Color.fromARGB(255, 104, 5, 190);
-static Color colorIconCalls = Color.fromARGB(255, 0, 0, 0);
-static Color colorIconCallsNested = Color.fromARGB(255, 9, 218, 187);
-
-
-
+//All Icon Color in Projects
+  static Color colorIconBack = Color.fromARGB(255, 104, 5, 190);
+  static Color colorIconSearch = Color.fromARGB(255, 104, 5, 190);
+  static Color colorIconCamera = Color.fromARGB(255, 104, 5, 190);
+  static Color colorIconcontact = Color.fromARGB(255, 104, 5, 190);
+  static Color colorIconCalls = Color.fromARGB(255, 0, 0, 0);
+  static Color colorIconCallsNested = Color.fromARGB(255, 9, 218, 187);
+  static Color colorIconViber = Color.fromARGB(255, 0, 0, 0);
 }
-
