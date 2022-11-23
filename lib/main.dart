195e663517e4d161b6a57951f0fc1882365696e5
Hana <afshari.hln@gmail.com>
2022-11-24 02:57:45 +0330

    update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:viber_getx/binding.dart';
 import 'package:viber_getx/component/theme_viber.dart';
 import 'package:viber_getx/model/message_model.dart';
 import 'package:viber_getx/model/user_inbox_list_model.dart';
+import 'package:viber_getx/view/chat_screen.dart';
 
 import 'package:viber_getx/view/contact_list_screen.dart';
 import 'package:viber_getx/view/main_screen.dart';
@@ -44,7 +45,7 @@ class MyApp extends StatelessWidget {
          GetPage(name: RouteCallsInViewAllRecentCalls, page: () => CallsInViewAllRecentCalls(),),
         GetPage(name: RouteContactListScreen, page: () => ContactListScreen(),binding: BindingContactController()),
        ],      
-      home:MusicScreen(),
+      home:ChatScreen(),
       
       
     );
